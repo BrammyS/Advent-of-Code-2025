1233340e8c70878bf7c92677cba0c84dd1e9c08b
BrammyS <bram.esendam@gmail.com>
2025-12-02 17:03:43 +0100

    Day 2 WIP :(
    ITS NOT WORKING

---
+++ src/Day2/Program.fs
@@ -0,0 +1,19 @@
+﻿let sumInvalidIds (idRanges: array<string>) : int =
+    let mutable sum = 0
+
+    for idRange in idRanges do
+        let parts = idRange.Split('-')
+        let ids = [ parts.[0] |> int .. parts.[1] |> int ]
+        
+        let isEven id = (id % 2) = 0
+        let amountOfDigits id = int (log10 (float id)) + 1
+        let idsToValidate = List.filter (fun x -> isEven (amountOfDigits x)) ids
+        
+        let firstPart id digits= (id / (pown 10 digits - 1) )
+        let test = firstPart 11 2
+        printfn $"%i{test}"
+        let validateId id digits = (id / (pown 10 digits - 1) ) = (id % (pown 10 digits - 1) )
+        let invalidIds = List.filter (fun x -> validateId x (amountOfDigits x)) idsToValidate
+        let newSum = List.sum invalidIds
+        sum <- sum + newSum
+    sum
---
+++ tests/Day2.UnitTests/UnitTest1.fs
@@ -0,0 +1,9 @@
+﻿module Day2
+
+open NUnit.Framework
+open Program
+open Shouldly
+
+[<Test>]
+let Test1 () =
+    sumInvalidIds([|"11-22"|]).ShouldBeEquivalentTo(33)
